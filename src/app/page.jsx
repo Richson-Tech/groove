96a6367f392f2adfafb65b3228d41e541f826b7b
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-09-11 15:18:09 +0100

    new featuress added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FaHotel } from "react-icons/fa";
+import { MdBorderColor } from "react-icons/md";
+import { SiGoogletranslate } from "react-icons/si";
+import { VscServerEnvironment } from "react-icons/vsc";
+import { BsEmojiSmileFill, BsCreditCard } from "react-icons/bs";
 
 const page = () => {
   return (
@@ -41,7 +45,10 @@ const page = () => {
           </div>
         </div>
       </div>
-      <div className="h-96 bg-white text-2xl mt-10">
+
+      {/* hospitabilities section */}
+
+      <div className="h-96 bg-white text-2xl mt-10 mb-60">
         <div className="flex items-center justify-center flex-wrap flex-col">
           <h2 className="font-['Work_Sans'] text-5xl">
             All In One Solution <br />
@@ -54,40 +61,172 @@ const page = () => {
 
         <div className="flex flex-row justify-between gap-5 mb-16">
           <div className="flex flex-1 flex-col pl-20">
-            <FaHotel/>
+            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-[#E5D9CE]">
+              <FaHotel size={25} className="text-[#AA825D]" />
+            </div>
             <h2>Prebuilt Websites</h2>
-            <p>All of the demo websites can be easily imported making it incredible easy to get started building your site.</p>
+            <p>
+              All of the demo websites can be easily imported making it
+              incredible easy to get started building your site.
+            </p>
           </div>
           <div className="flex flex-1 flex-col">
-            <FaHotel/>
+            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-[#E5D9CE]">
+              <MdBorderColor size={25} className="text-[#AA825D]" />
+            </div>
             <h2>Booking Management</h2>
-            <p>Accommodation availability is automatically managed and can be viewed in the calendar dashboard.</p>
+            <p>
+              Accommodation availability is automatically managed and can be
+              viewed in the calendar dashboard.
+            </p>
           </div>
           <div className="flex flex-1 flex-col">
-            <FaHotel/>
+            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-[#E5D9CE]">
+              <SiGoogletranslate size={25} className="text-[#AA825D]" />
+            </div>
             <h2>Language Translation</h2>
-            <p>A custom built multi language translation plugin is included allowing you to easily translate your site.</p>
+            <p>
+              A custom built multi language translation plugin is included
+              allowing you to easily translate your site.
+            </p>
           </div>
-          
         </div>
         <div className="flex flex-row justify-between gap-5 mb-20">
           <div className="flex flex-1 flex-col pl-20">
-            <FaHotel/> 
-            <h2>Drag & Drop Interface</h2>
-            <p>All pages and content can be edited using WP Bakery's drag and drop editor, no coding required.</p>
+            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-[#E5D9CE]">
+              <VscServerEnvironment size={25} className="text-[#AA825D]" />
+            </div>
+            <h2>Convinient Environment</h2>
+            <p>
+              All pages and content can be edited using WP Bakery's drag and
+              drop editor, no coding required.
+            </p>
           </div>
           <div className="flex flex-1 flex-col">
-            <FaHotel/>
+            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-[#E5D9CE]">
+              <BsEmojiSmileFill size={25} className="text-[#AA825D]" />
+            </div>
             <h2>Customer Friendly</h2>
-            <p>Looks great and functions perfectly on mobile phones, tablets, laptops, and desktop computers.</p>
+            <p>
+              Looks great and functions perfectly on mobile phones, tablets,
+              laptops, and desktop computers.
+            </p>
           </div>
           <div className="flex flex-1 flex-col">
-            <FaHotel/>
+            <div className="flex justify-center items-center w-16 h-16 rounded-full bg-[#E5D9CE]">
+              <BsCreditCard size={25} className="text-[#AA825D]" />
+            </div>
             <h2>Online Payments</h2>
-            <p>WooCommerce support allows you to collect credit card payments in a wide variety of local currencies.</p>
+            <p>
+              WooCommerce support allows you to collect credit card payments in
+              a wide variety of local currencies.
+            </p>
           </div>
         </div>
-          
+      </div>
+      {/* varies of rooms available  */}
+      <div className="h-max bg-black text-2xl mt-10">
+        <div className="flex items-center justify-center flex-wrap flex-col">
+          <h1 className="font-['Work_Sans'] text-5xl mt-5 text-white mb-5">
+            Demo Of Rooms
+          </h1>
+          <h6 className="text-white mb-10 font-[crimson]">The best experience you ever crave for Because comfort is more important<br /><span className="flex items-center justify-center">to see life from a different perspective.</span></h6>
+        </div>
+
+        <div className="flex flex-row justify-between gap-5">
+          <div className="flex flex-col flex-1">
+            <Link href="/">
+              <Image src="https://images.unsplash.com/photo-1608198399988-341f712c3711?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGhvdGVsJTIwcm9vbXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60" 
+              width={500}
+              height={500}
+              alt="Hotel room"
+              className="rounded-md "
+              />
+            <p className="flex items-center justify-center text-white font-['Work_Sans'] pt-5">standard Hotel</p>
+            </Link>
+          </div>
+          <div className="flex flex-col flex-1">
+            <Link href="/">
+              <Image src="https://images.unsplash.com/photo-1507038772120-7fff76f79d79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGhvdGVsJTIwcm9vbXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60" 
+              width={500}
+              height={500}
+              alt="Hotel room"
+              className="rounded-md "
+              />
+            <p className="flex items-center justify-center text-white font-['Work_Sans'] pt-5">Apartment</p>
+            </Link>
+          </div>
+          <div className="flex flex-col flex-1">
+            <Link href="/">
+              <Image src="https://images.unsplash.com/photo-1621293954908-907159247fc8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGhvdGVsJTIwcm9vbXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60" 
+              width={500}
+              height={500}
+              alt="Hotel room"
+              className="rounded-md"
+              />
+            <p className="flex items-center justify-center text-white font-['Work_Sans'] pt-5">Apartment 2</p>
+            </Link>
+          </div>
+          <div className="flex flex-col flex-1">
+            <Link href="/">
+              <Image src="https://images.unsplash.com/photo-1594560913095-8cf34bab82ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aG90ZWwlMjByb29tc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" 
+              width={500}
+              height={500}
+              alt="Hotel room"
+              className="rounded-md"
+              />
+            <p className="flex items-center justify-center text-white font-['Work_Sans'] pt-5">Bed & Breakfast</p>
+            </Link>
+          </div>
+        </div>
+
+
+        <div className="flex flex-row justify-between gap-5 mt-10">
+          <div className="flex flex-col flex-1">
+            <Link href="/">
+              <Image src="https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGhvdGVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" 
+              width={500}
+              height={500}
+              alt="Hotel room"
+              className="rounded-md"
+              />
+            <p className="flex items-center justify-center text-white font-['Work_Sans'] pt-5">town Villa</p>
+            </Link>
+          </div>
+          <div className="flex flex-col flex-1">
+            <Link href="/">
+              <Image src="https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvdGVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" 
+              width={500}
+              height={500}
+              alt="Hotel room"
+              className="rounded-md"
+              />
+            <p className="flex items-center justify-center text-white font-['Work_Sans'] pt-5">city Hotel</p>
+            </Link>
+          </div>
+          <div className="flex flex-col flex-1">
+            <Link href="/">
+              <Image src="https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" 
+              width={500}
+              height={500}
+              alt="Hotel room"
+              className="rounded-md"
+              />
+            <p className="flex items-center justify-center text-white font-['Work_Sans'] pt-5">beach Resort</p>
+            </Link>
+          </div>
+          <div className="flex flex-col flex-1">
+            <Link href="/">
+              <Image src="https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" 
+              width={500}
+              height={500}
+              alt="Hotel room"
+              className="rounded-md"
+              />
+            <p className="flex items-center justify-center text-white font-['Work_Sans'] pt-5">boutique Villa</p>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='mt-20'>
-        <div className='bg-black h-20 text-white'>
+        <div className='bg-black h-20 text-white mt-3'>
    Footer
         </div>
        
