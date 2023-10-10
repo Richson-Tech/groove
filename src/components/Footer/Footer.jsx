9de979bdc7d6ceca8667fb9892cf09839b58e565
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-10-10 12:29:41 +0100

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,9 @@ const Footer = () => {
     <div className="mt-20">
       <div className="bg-black h-48">
         <div className="flex flex-row items-center justify-center text-white shadow-black mt-3">
-    <h1 className="flex flex-row items-center justify-center">Footer</h1>
+    <h1 className="flex flex-row items-center justify-center text-3xl mt-20 font-['Work_Sans'] ">
+    Copyright Richson Tech 2023
+      </h1>
         </div>
 
         </div>
