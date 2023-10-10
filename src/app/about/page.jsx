7b00a698675a12b5dbd38b7027d21346e95e6c1a
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-10-10 16:55:40 +0100

    about page progress

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 const About = () => {
   return (
-    <div>About Us</div>
+    <div className='h-max'>
+      <div className=''>
+    <Image className='h-52' src="https://plus.unsplash.com/premium_photo-1661676056771-f6c2711249e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGhvdGVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+    width={1920}
+    height={500}
+    />
+      </div>
+    </div>
   )
 }
 
